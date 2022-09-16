98855001b1b816b30137259443419296b6b4e2ff
SHINYA <ttiuweqpp2901@gmail.com>
2022-09-16 21:27:58 +0900

    [fix]中国フォントを日本語フォントに修正

--- lib/presentation/parts/wordstock_theme.dart.dart
+++ lib/presentation/parts/wordstock_theme.dart.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WordStockTheme {
   static ThemeData light() {
     return ThemeData(
+      textTheme: GoogleFonts.sawarabiGothicTextTheme(),
       primarySwatch: Colors.blue,
       scaffoldBackgroundColor: const Color.fromARGB(255, 151, 181, 238),
     );
